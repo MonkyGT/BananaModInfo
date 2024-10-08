4d46cb26f0201e40a9625f88a8c201a969696a64
DeveloperPixel0 <coreypolley81@gmail.com>
2024-10-07 20:29:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function isNumeric(value) {
 }
 
 modList.mods.forEach(mod => 
-  https.get(`${prefix}${!isNumeric(mod.gitPath) ? repos : repositories}${mod.gitPath}${postfix}`, { headers: { 'User-Agent' : 'DeadlyKitten/MonkeModInfo' ,'Authorization': `Token ${process.env.SECRET}`}},(res) => {
+    https.get(`${prefix}${!isNumeric(mod.gitPath) ? repos : repositories}${mod.gitPath}${postfix}`, { headers: { 'User-Agent': 'DeveloperPixel0/BananaModInfo' ,'Authorization': `Token ${process.env.SECRET}`}},(res) => {
     let body = "";
 
       res.on("data", (chunk) => {
